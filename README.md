cb3534253ae256d52e46308a923aa292b8dbaa18
Yosuke Tsuboi <ytsuboi@ytsuboi.nrt.csb>
2022-11-18 14:00:20 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# workshop-refactoring
---
+++ src/main/java/sandbox/StockManager.java
@@ -0,0 +1,62 @@
+package sandbox;
+
+class StockManager {
+    Item[] items;
+
+    public StockManager(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            if (!items[i].name.equals("ビンテージワイン")
+                    && !items[i].name.equals("サッカー観戦チケット")) {
+                if (items[i].quality > 0) {
+                    if (!items[i].name.equals("ルビーの指輪")) {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name.equals("サッカー観戦チケット")) {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!items[i].name.equals("ルビーの指輪")) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (items[i].sellIn < 0) {
+                if (!items[i].name.equals("ビンテージワイン")) {
+                    if (!items[i].name.equals("サッカー観戦チケット")) {
+                        if (items[i].quality > 0) {
+                            if (!items[i].name.equals("ルビーの指輪")) {
+                                items[i].quality = items[i].quality - 1;
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
